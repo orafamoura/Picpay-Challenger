ad9f0c5b6043e418762e178a0b39fb01f9c4e914
Raphael moura <raphaelmvb2@gmail.com>
2023-10-17 22:00:18 -0300

    Created User service

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -15,10 +15,19 @@ public class UserService {
     @Autowired // faz a injecao automatica
     private UserRepository repository;
 
-    public void validateTransaction(User sender, BigDecimal amount) {
-        if(sender.getUserType() == UserType.COMMON) {
-
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception { //posteriormente implementar tratamento de excecoes
+        if(sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("User type is not authorized to carry out the transaction");
+        }
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("User not found enough");
         }
     }
+    public User findUserById(Long id) throws Exception { //criamos para o TransactionService nao ter acesso direto ao user repository, ele so vai manipular o transaction repository
+       return this.repository.findUserById(id).orElseThrow(() -> new Exception("User no found")); // se nao encontrar o usuario lancamos uma exception
+    }
 
+    public void saveUser(User user){ //so para persistir as alteracoes do usuario
+        this.repository.save(user);
+    }
 }
